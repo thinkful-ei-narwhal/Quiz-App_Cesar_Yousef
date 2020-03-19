866ab2b9ed3575146854c42ac7aa3ea3e27f9b66
Cesar Morales <cesarlenin2@live.com>
2020-03-19 13:12:07 -0700

    added a render fuction and starttemplate function

--- app.js
+++ app.js
@@ -1,36 +1,37 @@
 /**
  * Example store structure
  */
+// eslint-disable-next-line strict
 const store = {
-    // 5 or more questions are required
-    questions: [
-      {
-        question: 'What color is broccoli?',
-        answers: [
-          'red',
-          'orange',
-          'pink',
-          'green'
-        ],
-        correctAnswer: 'green'
-      },
-      {
-        question: 'What is the current year?',
-        answers: [
-          '1970',
-          '2015',
-          '2019',
-          '2005'
-        ],
-        correctAnswer: '2019'
-      }
-    ],
-    quizStarted: false,
-    questionNumber: 0,
-    score: 0
-  };
+  // 5 or more questions are required
+  questions: [
+    {
+      question: 'What color is broccoli?',
+      answers: [
+        'red',
+        'orange',
+        'pink',
+        'green'
+      ],
+      correctAnswer: 'green'
+    },
+    {
+      question: 'What is the current year?',
+      answers: [
+        '1970',
+        '2015',
+        '2019',
+        '2005'
+      ],
+      correctAnswer: '2019'
+    }
+  ],
+  quizStarted: false,
+  questionNumber: 0,
+  score: 0
+};
   
-  /**
+/**
    * 
    * Technical requirements:
    * 
@@ -44,15 +45,41 @@ const store = {
    * SEE BELOW FOR THE CATEGORIES OF THE TYPES OF FUNCTIONS YOU WILL BE CREATING 👇
    * 
    */
+/********** TEMPLATE GENERATION FUNCTIONS **********/
+// These functions return HTML templates
+
+//fuction that choices which template
+//functions that writes the template---startpg, questionpg, resultspg, finalpg
+//function that render the pg
+
+function renderTemplate(){
+  console.log('`renderTemplate` ran');
+  const renderTemplateString = startQuizElement ();
+  $('main').html(renderTemplateString);
+}
+
+function startQuizElement () {
+  return `
+  <section class="quiz-explanation">
+    <p class="quiz-description">anything can go here this is just a example</p>
+  <section>
+  <section class="button-section">
+    <button class="js-button">
+    <span class="button-label">yes</span>
+    </section>`;
+}
+
+function handleQuizapp(){
+  renderTemplate();
+  startQuizElement ();
+}
   
-  /********** TEMPLATE GENERATION FUNCTIONS **********/
+/********** RENDER FUNCTION(S) **********/
   
-  // These functions return HTML templates
+// This function conditionally replaces the contents of the <main> tag based on the state of the store
   
-  /********** RENDER FUNCTION(S) **********/
+/********** EVENT HANDLER FUNCTIONS **********/
   
-  // This function conditionally replaces the contents of the <main> tag based on the state of the store
-  
-  /********** EVENT HANDLER FUNCTIONS **********/
-  
-  // These functions handle events (submit, click, etc)
+// These functions handle events (submit, click, etc)
+
+$(handleQuizapp);
